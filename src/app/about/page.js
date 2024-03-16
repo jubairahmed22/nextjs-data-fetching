c41c4574d9cf685c729a7c6c46b24f42ff637147
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-17 00:39:13 +0600

    image font and seo

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "about page",
+    description: "this is about page"
+  }
+  
+const AboutPage = () => {
+    return (
+        <div>
+            this is about page
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/gallery/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import nextImg from "../../assets/nextjs-img.webp";
+
+const GalleryPage = () => {
+    return (
+        <div>
+            <h1 className="text-xl text-center">Image optimization</h1>
+            <h1 className="text-xl mt-5 text-center">Regular image teg</h1>
+            <img width={500} height={500} src="https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg" alt="next-image" className="mx-auto"></img>
+            <h1 className="text-xl mt-5 text-center">Nextjs  image teg</h1>
+
+            <Image src="https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg" alt="" width={500} height={500} className="mx-auto"/>
+            <h1 className="text-xl mt-5 text-center">Local  image teg</h1>
+
+            <Image src={nextImg} alt="" width={500} height={500} className="mx-auto"/>
+        </div>
+    );
+};
+
+export default GalleryPage;
